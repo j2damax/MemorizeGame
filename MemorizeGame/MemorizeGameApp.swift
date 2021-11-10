6e28b1903c57dc77098707984cb166dc72b53748
Jayampathy Balasuriya <jayampathy@gapstars.net>
2021-11-10 06:36:16 +0530

    initial project added

---
+++ MemorizeGame/MemorizeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeGameApp.swift
+//  MemorizeGame
+//
+//  Created by Jayampathy Balasuriya on 2021-11-10.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
